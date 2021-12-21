a95158ac32d8b039c4207703a6d3c56f000d1d27
alonatt <alondra08@ug.uchile.cl>
2021-12-21 00:26:04 +0000

    mirepo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,64 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateRandom(array) {
+  let rango_maximo = array.length;
+  let aleatorio = Math.floor(Math.random() * (rango_maximo - 0) + 0);
+  return aleatorio;
+}
+
+function generarOracion() {
+  let who = [
+    "Mi perro",
+    "Mi gato",
+    "Mi hija",
+    "Mi amigo",
+    "Mi mamá",
+    "Mi papá",
+    "Mi vecino"
+  ];
+  let action = ["guardo", "rego", "salto", "lloro", "jugo"];
+  let what = [
+    "mi tarea",
+    "mi trabajo",
+    "el reloj ",
+    "la piscina",
+    "la casa",
+    "el auto"
+  ];
+  let when = [
+    "ayer",
+    "en las vaciones",
+    "cuando fue al negocio",
+    "en su fiesta ",
+    "cuando dormia"
+  ];
+  let oracion =
+    who[generateRandom(who)] +
+    " " +
+    action[generateRandom(action)] +
+    " " +
+    what[generateRandom(what)] +
+    " " +
+    when[generateRandom(when)];
+  return oracion;
+}
+const boton = document.getElementById("boton");
+let contador = 0;
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#Generador").innerHTML = generarOracion();
+};
+
+boton.onclick = function() {
+  contador += 1;
+  if (contador >= 5) {
+    alert("No hay excusas");
+    document.querySelector("#Generador").innerHTML = "Se acabaron las excusas";
+    boton.innerText = "No hay mas excusas";
+    boton.disabled = true;
+  } else {
+    alert("Ya Van: " + contador + " excusas");
+    document.querySelector("#Generador").innerHTML = generarOracion();
+  }
 };
